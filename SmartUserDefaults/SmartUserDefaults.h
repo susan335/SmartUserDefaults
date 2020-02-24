9b84a432ef4612d807b141b903ef4db8bcffd23d
watanave <susan.se3p@gmail.com>
2020-02-24 21:10:27 +0900

    Initial commit

---
+++ SmartUserDefaults/SmartUserDefaults.h
@@ -0,0 +1,19 @@
+//
+//  SmartUserDefaults.h
+//  SmartUserDefaults
+//
+//  Created by Yohta Watanave on 2020/02/24.
+//  Copyright © 2020 Yohta Watanave. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SmartUserDefaults.
+FOUNDATION_EXPORT double SmartUserDefaultsVersionNumber;
+
+//! Project version string for SmartUserDefaults.
+FOUNDATION_EXPORT const unsigned char SmartUserDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartUserDefaults/PublicHeader.h>
+
+
---
+++ SmartUserDefaults/SmartUserDefaults.swift
@@ -0,0 +1,77 @@
+//
+//  SmartUserDefaults.swift
+//  SmartUserDefaults
+//
+//  Created by Yohta Watanave on 2020/02/24.
+//  Copyright © 2020 Yohta Watanave. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct UserDefaultsProperty<T> where T: Any {
+    private let userDefaults: UserDefaults
+    public let key: String
+    private let defaultValue: T
+    
+    public init(userDefaults: UserDefaults = UserDefaults.standard, key: String, defaultValue: T) {
+        self.userDefaults = userDefaults
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+        
+    public var wrappedValue: T {
+        get {
+            if T.self == URL.self {
+                return userDefaults.url(forKey: key) as? T ?? defaultValue
+            }
+            else {
+                return userDefaults.object(forKey: key) as? T ?? defaultValue
+            }
+        }
+        set {
+            if let newUrl = newValue as? URL {
+                userDefaults.set(newUrl, forKey: key)
+            }
+            else {
+                userDefaults.set(newValue, forKey: key)
+            }
+        }
+    }
+}
+
+@propertyWrapper
+public struct UserDefaultsOptionalProperty<T> {
+    private let userDefaults: UserDefaults
+    public let key: String
+    
+    public init(userDefaults: UserDefaults = UserDefaults.standard, key: String) {
+        self.userDefaults = userDefaults
+        self.key = key
+    }
+        
+    public var wrappedValue: T? {
+        get {
+            if T.self == URL.self {
+                return userDefaults.url(forKey: key) as? T
+            }
+            else {
+                return userDefaults.object(forKey: key) as? T
+            }
+        }
+        set {
+            if let newValue = newValue {
+                if let newUrl = newValue as? URL {
+                    userDefaults.set(newUrl, forKey: key)
+                }
+                else {
+                    userDefaults.set(newValue, forKey: key)
+                }
+                
+            }
+            else {
+                userDefaults.removeObject(forKey: key)
+            }
+        }
+    }
+}
